7c9abd46adc9dd96668827b3118cd513998b47c0
Diogo Lisboa Freire de Araujo <diogolisboa.araujo@gmail.com>
2021-10-25 16:59:30 -0300

    Lista de tarefas
    Adicionando o programa de lista de tarefas

---
+++ lista_tarefas/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+
+    return const MaterialApp(
+      home: Random(),
+    );
+  }
+}
+
+class Random extends StatefulWidget {
+  const Random({Key? key}) : super(key: key);
+
+  @override
+  _RandomState createState() => _RandomState();
+}
+
+class _RandomState extends State<Random> {
+  final _suggestions = <WordPair> [];
+  final _biggerFonts = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: _buildSuggestions(),
+    );
+  }
+
+  //Cria a lista e determina que ela adicione mais elementos da _buildRow caso
+  //o index seja menor que o numero de sugestões carregadas
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      addAutomaticKeepAlives: false,
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+        final int index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  //Cria os elementos da lista com a função WordPair do pacote english_words
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        //asPacalCase = UpperCamelCase
+        style: _biggerFonts,
+      ),
+    );
+  }
+
+  void _showDialog(){
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return const AlertDialog(
+          title: Text('Alert Title'),
+          content: Text('Alert body'),
+        );
+      }
+    );
+  }
+
+}
