b83ce5b6ccafa03bcb21d5d184a124f699a57164
Diogo Lisboa Freire de Araujo <diogolisboa.araujo@gmail.com>
2021-11-19 10:05:08 -0300

    Added updateAlbum()
    adicionado updateAlbum, textfield e botão para mudar texto puxado pelo Json / Added updateAlbum, textfield and button to change text requested through Json

--- Fetch Json/main.dart
+++ Fetch Json/main.dart
@@ -4,31 +4,43 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> fetchAlbum() async {
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+Future<Album> updateAlbum(String title) async {
+  final response = await http.put(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    body: jsonEncode(<String, String>{
+      'title': title,
+    }),
+  );
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to update album');
+  }
+}
 
-  if (response.statusCode == 200){
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+
+  if (response.statusCode == 200) {
     return Album.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load album');
   }
 }
 
-class Album{
-  final int userId;
-  final int id;
+class Album {
   final String title;
 
   Album({
-    required this.userId,
-    required this.id,
     required this.title,
-});
+  });
 
-  factory Album.fromJson(Map<String, dynamic> json){
+  factory Album.fromJson(Map<String, dynamic> json) {
     return Album(
-      userId: json['userId'],
-      id: json['id'],
       title: json['title'],
     );
   }
@@ -48,15 +60,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const MyAlbum(taitoru: 'A-album'),
+      home: const MyAlbum(title: 'A-album'),
     );
   }
 }
 
-
 class MyAlbum extends StatefulWidget {
-  const MyAlbum({Key? key, required this.taitoru}) : super(key: key);
-  final String taitoru;
+  const MyAlbum({Key? key, required this.title}) : super(key: key);
+  final String title;
 
   @override
   _MyAlbumState createState() => _MyAlbumState();
@@ -64,6 +75,7 @@ class MyAlbum extends StatefulWidget {
 
 class _MyAlbumState extends State<MyAlbum> {
   late Future<Album> futureAlbum;
+  final TextEditingController _textController = TextEditingController();
 
   @override
   void initState() {
@@ -71,38 +83,72 @@ class _MyAlbumState extends State<MyAlbum> {
     futureAlbum = fetchAlbum();
   }
 
+  updateData(){
+    futureAlbum = updateAlbum(_textController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.taitoru),
-      ),
-      body: Center(
-        child: Card(
-          elevation: 10,
-          child:Padding(
-            padding: EdgeInsets.all(10),
-            child:FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot){
-              if (snapshot.hasData){
-                return Text(
-                  snapshot.data!.title,
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.deepPurple,
-                    backgroundColor: Colors.white10,
-                  ),
-                );
-              } else if (snapshot.hasError){
-                return Text('${snapshot.error}');
-              }
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(children: [
+            Card(
+                margin: EdgeInsets.only(top: 15),
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: FutureBuilder<Album>(
+                      future: futureAlbum,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(
+                            snapshot.data!.title,
+                            style: const TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              color: Colors.deepPurple,
+                              backgroundColor: Colors.white10,
+                            ),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text('${snapshot.error}');
+                        }
 
-              return const CircularProgressIndicator();
-            }),))
-      ),
-    );
+                        return const CircularProgressIndicator();
+                      }),
+                )),
+            Padding(
+                padding: const EdgeInsets.all(25),
+                child: TextField(
+                  controller: _textController,
+                  onSubmitted:(){
+                    setState(() {
+                      updateData();
+                    });
+                  }(),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Change the text',
+                    fillColor: Colors.deepPurple,
+                  ),
+                )),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  updateData();
+                });
+              },
+              child: const Text('Update data'),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepPurple,
+                elevation: 5,
+              ),
+            )
+          ]),
+        ));
   }
 }
